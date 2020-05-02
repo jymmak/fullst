05c6a2edc4ddfe89af391a1a6f60185dbb9d05e3
jymma karina mogollon cardoza <jkmogollon.cp@gmail.com>
2020-05-02 10:05:43 -0500

    up files angular

---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'Proyecto 1';
+  description = 'Uso de angular, spring boot y postgresql'
+}
---
+++ src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { CreateCustomerComponent } from './create-customer/create-customer.component';
+import { CustomerDetailsComponent } from './customer-details/customer-details.component';
+import { CustomersListComponent } from './customers-list/customers-list.component';
+import { SearchCustomersComponent } from './search-customers/search-customers.component';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CreateCustomerComponent,
+    CustomerDetailsComponent,
+    CustomersListComponent,
+    SearchCustomersComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    AppRoutingModule,
+    HttpClientModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
